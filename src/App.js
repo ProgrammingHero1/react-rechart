dc34fbcac140a7966d1471412b8540aabba8a551
Programming Hero <programming.hero1@gmail.com>
2022-03-11 22:50:38 -0600

    react spring

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import MyLineChart from './components/MyLineChart/MyLineChart';
 import SpecialChart from './components/SpecialChart/SpecialChart';
+import { useSpring, animated } from 'react-spring'
+import { useState } from 'react';
 
 function App() {
+  const [flip, set] = useState(false)
+  const props = useSpring({ 
+    to: { opacity: 1 }, 
+    from: { opacity: 0 },
+    reset: true,
+    reverse: flip,
+    delay: 200, 
+    onRest: () => set(!flip),
+  })
+
   return (
     <div className="App">
+      <animated.div style={props}>I will fade in</animated.div>
       <MyLineChart></MyLineChart>
       <SpecialChart></SpecialChart>
     </div>
